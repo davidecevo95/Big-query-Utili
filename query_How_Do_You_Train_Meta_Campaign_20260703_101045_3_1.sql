cbf9d3c9cefb50e9482ee59c433fa6dcdab2a8f9
davidecevo95 <160876797+davidecevo95@users.noreply.github.com>
2026-07-03 10:23:21 +0200

    Delete query_How_Do_You_Train_Meta_Campaign_20260703_101045_3_1.sql

--- query_How_Do_You_Train_Meta_Campaign_20260703_101045_3_1.sql
+++
@@ -1,3 +0,0 @@
-Mi dispiace, ma le immagini che hai fornito non contengono uno screenshot di una query SQL di BigQuery. Le immagini mostrano il ritratto di una persona su uno sfondo giallo.
-
-Pertanto, non posso trascrivere alcuna query SQL né fornirti i commenti richiesti. Se hai un'immagine con una query SQL, per favore, caricala e sarò felice di aiutarti.
