b376d08a5218b4eddbe4e167f0b0467345f6ee4d
davidecevo95 <160876797+davidecevo95@users.noreply.github.com>
2026-07-03 10:00:38 +0200

    Delete query_The_Aggregated_Report_Looks_Gr_20260702_172836_1_3.sql

--- query_The_Aggregated_Report_Looks_Gr_20260702_172836_1_3.sql
+++
@@ -1,7 +0,0 @@
-L'immagine fornita non contiene una query SQL di BigQuery. Mostra invece due grafici a linee che rappresentano tendenze di dati.
-
--- L'immagine fornita non contiene una query SQL di BigQuery.
--- Mostra due grafici a linee separati per "App" e "Web".
--- Il grafico "App" (linea gialla) mostra un andamento fluttuante dei dati nel tempo.
--- Il grafico "Web" (linea blu) mostra un andamento più costante e in crescita dei dati nel tempo.
--- Non sono presenti calcoli SQL o una query da trascrivere o spiegare.
