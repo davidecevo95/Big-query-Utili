b310626623a4f1f6e9a0cfe9fe96d2f125cacbda
davidecevo95 <160876797+davidecevo95@users.noreply.github.com>
2026-07-03 09:54:16 +0200

    Delete query_Consent_Mode_Is_On___But_Your__20260702_172937_2_1.sql

--- query_Consent_Mode_Is_On___But_Your__20260702_172937_2_1.sql
+++
@@ -1,3 +0,0 @@
-Mi dispiace, ma l'immagine fornita è completamente bianca e non contiene uno screenshot di una query SQL. Non riesco a vedere alcun codice da trascrivere o analizzare.
-
-Per favore, potresti ricaricare l'immagine corretta con lo screenshot della query SQL di BigQuery, oppure fornirmi direttamente il testo della query? Sarò felice di aiutarti una volta che avrò il contenuto su cui lavorare.
