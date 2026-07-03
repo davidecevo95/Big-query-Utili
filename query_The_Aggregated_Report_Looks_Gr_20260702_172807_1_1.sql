c5fda906a1531c4b4081287e8d3ecca91f90d75b
davidecevo95 <160876797+davidecevo95@users.noreply.github.com>
2026-07-03 09:55:36 +0200

    Delete query_The_Aggregated_Report_Looks_Gr_20260702_172807_1_1.sql

--- query_The_Aggregated_Report_Looks_Gr_20260702_172807_1_1.sql
+++
@@ -1,5 +0,0 @@
-Mi dispiace, ma l'immagine fornita è completamente bianca e non contiene uno screenshot di una query SQL di BigQuery.
-
-Di conseguenza, non sono in grado di trascrivere la query né di fornirne un'analisi.
-
-Ti prego di caricare l'immagine corretta contenente la query SQL per poter procedere con la tua richiesta.
