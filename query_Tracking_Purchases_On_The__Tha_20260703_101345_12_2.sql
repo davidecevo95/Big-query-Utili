605c018fbc48160f23f8337f16f945bf7246e1c6
davidecevo95 <160876797+davidecevo95@users.noreply.github.com>
2026-07-03 10:13:45 +0200

    Backup automatico query - Tracking Purchases On The “Thank You” Pa

---
+++ query_Tracking_Purchases_On_The__Tha_20260703_101345_12_2.sql
@@ -0,0 +1,3 @@
+L'immagine fornita non contiene uno screenshot di una query SQL di BigQuery, ma piuttosto un messaggio di ringraziamento e una richiesta di controllare l'email per maggiori informazioni su un ordine.
+
+Di conseguenza, non posso trascrivere una query SQL o fornire commenti su di essa.
