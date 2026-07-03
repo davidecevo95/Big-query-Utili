1c2431c6b7a74b1e9855c2108d90bf2250fdf26d
davidecevo95 <160876797+davidecevo95@users.noreply.github.com>
2026-07-03 10:13:34 +0200

    Backup automatico query - Tracking Purchases On The “Thank You” Pa

---
+++ query_Tracking_Purchases_On_The__Tha_20260703_101334_12_1.sql
@@ -0,0 +1,3 @@
+L'immagine fornita è un ritratto di una persona e non contiene uno screenshot di una query SQL di BigQuery. Non è presente alcun codice SQL da trascrivere o commentare.
+
+Per favore, fornisci un'immagine che contenga effettivamente una query SQL se desideri che venga analizzata.
