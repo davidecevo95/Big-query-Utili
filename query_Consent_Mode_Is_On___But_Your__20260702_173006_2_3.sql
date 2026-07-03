2698744f48aaad758b0a1ca3de78fbacf341a9cc
davidecevo95 <160876797+davidecevo95@users.noreply.github.com>
2026-07-03 09:55:10 +0200

    Delete query_Consent_Mode_Is_On___But_Your__20260702_173006_2_3.sql

--- query_Consent_Mode_Is_On___But_Your__20260702_173006_2_3.sql
+++
@@ -1,3 +0,0 @@
-Ho analizzato l'immagine fornita. Sembra che l'immagine sia uno screenshot dell'interfaccia di Google Tag Manager, mostrando un elenco di tag, i loro tipi, i trigger di attivazione e la data dell'ultima modifica.
-
-Tuttavia, l'immagine *non contiene* una query SQL di BigQuery. Pertanto, non posso trascrivere alcuna query né aggiungere i commenti SQL richiesti.
